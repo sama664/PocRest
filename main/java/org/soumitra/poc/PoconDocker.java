182ca49d7e63bf341e99668d12c18ee7f3ac68cb
soumitrabanerjee <sama_664@yahoo.co.in>
2022-09-14 20:58:40 -0400

    Added added main class

---
+++ main/java/org/soumitra/poc/PoconDocker.java
@@ -0,0 +1,12 @@
+package org.soumitra.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoconDocker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoconDocker.class);
+    }
+}
